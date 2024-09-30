477bc5aa0d540670dd87b8beeb9009de7efe529e
Helena <helena@klarr.se>
2024-09-30 10:52:58 +0200

    finished enum example 1 and 2

--- src/menu_type/Main.java
+++ src/menu_type/Main.java
@@ -1,4 +1,23 @@
 package menu_type;
 
 public class Main {
+    public static void main(String[] args) {
+        Menu breakfastMenu = new Menu(MenuType.BREAKFAST);
+        breakfastMenu.addDish("Pancakes");
+        breakfastMenu.addDish("Scrambled eggs");
+        breakfastMenu.addDish("Toast");
+
+        Menu lunchMenu = new Menu(MenuType.LUNCH);
+        lunchMenu.addDish("Chicken salad");
+        lunchMenu.addDish("Chicke wrap");
+
+        Menu dinnerMenu = new Menu(MenuType.DINNER);
+        dinnerMenu.addDish("Steak with fries");
+
+        breakfastMenu.printMenuDetails();
+        System.out.println("--------------------------");
+        lunchMenu.printMenuDetails();
+        System.out.println("--------------------------");
+        dinnerMenu.printMenuDetails();
+    }
 }
